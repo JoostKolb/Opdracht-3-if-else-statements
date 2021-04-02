4f6107eeb626b16e83d07a9b1ef452f719b986c9
Joost Kolb <joostkolb@hotmail.com>
2021-04-02 19:52:38 +0200

    Add files via upload

---
+++ Opdracht 3 - if else statements/script.js
@@ -0,0 +1,35 @@
+// Deel 1
+const age = 21;
+
+if (age >= 21) {
+
+    console.log("Je bent oud genoeg");
+
+} else {
+
+    console.log("Je bent te jong, je moet helaas buiten blijven");
+
+}
+
+// Deel 2
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log("Je bent een vrouw, kom binnen.");
+
+}
+
+// Deel 3
+const driverStatus = "dronken";
+
+if (driverStatus == "bob") {
+
+
+    console.log("Je mag nog rijden");
+
+} else {
+
+    console.log("Je bent onder invloed en mag niet meer rijden.");
+
+}
